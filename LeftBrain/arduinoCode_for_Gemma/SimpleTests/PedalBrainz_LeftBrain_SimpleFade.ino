2cba0868f2789e6dbb3068bacc1aa266979c7db5
PedalBrainz <53237063+lostchocolatelab@users.noreply.github.com>
2021-05-01 09:48:34 -0700

    Update PedalBrainz_LeftBrain_SimpleFade.ino
    Replaced AnalogIO and finished Fade in/out Time, Delay while Bright and Dark

--- LeftBrain/arduinoCode_for_Gemma/SimpleTests/PedalBrainz_LeftBrain_SimpleFade.ino
+++ LeftBrain/arduinoCode_for_Gemma/SimpleTests/PedalBrainz_LeftBrain_SimpleFade.ino
@@ -1,44 +1,77 @@
-#include <AnalogIO.h>
+/**
+
+  Simple Fade In/Out with Delays of the onboard DotStar LED for the Adafruit Gemma M0
+  This sketch is good for controlling the speed of fade in/ out and delay times while fully bright and dark.
+
+  -Pedal Brainz
+
+*/
+
 #include <Adafruit_DotStar.h>
 
-/* Set the DtoStar as "pixel". */
+/* Set the on-board DotStar as "pixel". */
 Adafruit_DotStar pixel = Adafruit_DotStar(1, INTERNAL_DS_DATA, INTERNAL_DS_CLK, DOTSTAR_BGR);
 
-/* Creating the object for reading the value from a potentiometer. */
-AnalogIn potentiometerA0(A0, BIT8);
-AnalogIn potentiometerA1(A1, BIT8);
-AnalogIn potentiometerA2(A2, BIT8);
+// Declare the potentiometer variables
+int valueA0;
+int valueA1;
+int valueA2;
 
-/* Set a value for each potentiometer (pin). */
-unsigned short valuePotentiometerA0 = 0;
-unsigned short valuePotentiometerA1 = 0;
-unsigned short valuePotentiometerA2 = 0;
+// Set a default variable for the red, blue, and green (RGB) value of the on-board Dot Star
+// Pink! Potentiometer A0: 236 Potentiometer A1: 73 Potentiometer A2: 107
+
+int redValue = 236;
+int blueValue = 73;
+int greenValue = 107;
 
 
 void setup() {
-    Serial.begin(57600);
-    /* Start the DotStar LED */
-    pixel.begin();
-    //pixel.setBrightness(155);
-    pixel.setPixelColor(0, 50, 255, 100); 
-    pixel.show(); 
-    
+  Serial.begin(57600);
+  /* Start the DotStar LED */
+  pixel.begin();
+  pixel.setBrightness(255);
+  pixel.setPixelColor(0, redValue, blueValue, greenValue);
+  pixel.show();
+
 }
 
 void loop() {
   int x = 1;
+
+  //This fades the brightness in and out using maths
   for (int i = 0; i > -1; i = i + x) {
     pixel.setBrightness(i);
-    pixel.show(); 
-    valuePotentiometerA2 = potentiometerA2.read();
-    Serial.println(valuePotentiometerA2);
+    pixel.show();
+
+
+    //If the fade has reached it's peak keep the LED lit and then fade out
+
     if (i == 50) {
+
+      //Potentiometer Top Right | A1 - Map the value of the potentiometer from 0-1024 to 0-5000 to the variable valueA1
+      valueA1 = map(analogRead(A1), 0, 1024, 0, 5000);
+
+      //Use the value of A1 to keep the LED fully lit by the amount of A1 (delay) and then start fading out
+      delay(valueA1);
+      Serial.println("Delay when LED fully lit (ms): " + String(valueA1));
+
       x = -1;  // switch direction at peak
     }
-    valuePotentiometerA0 = potentiometerA0.read();
-    delay(valuePotentiometerA0/1.9);
-    
+
+    //Potentiometer Top Right | A0 - Map the value of the potentiometer from 0-1024 to 0-135 to the variable valueA0
+    valueA0 = map(analogRead(A0), 0, 1024, 2, 135);
+
+    //Use the value of A0 to control the rate of fade in/out using a delay
+    delay(valueA0);
   }
-  valuePotentiometerA1 = potentiometerA1.read();
-  delay(valuePotentiometerA1*20);
+  Serial.println("Rate of Fade in/ Fade out: " + String(valueA0));
+
+  //Potentiometer Bottom Right | A2 - Map the value of the potentiometer from 0-1024 to 0-5000 to the variable valueA2
+  valueA2 = map(analogRead(A2), 0, 1024, 0, 5000);
+
+  //Use the value of A2 to keep the LED fully dark (delay) between fade in/fade out
+  delay(valueA2);
+  Serial.println("Delay when LED fully dark (ms): " + String(valueA2));
+
+  //Serial.println("Rate of Fade in/ Fade out: " + String(valueA0) + " ||| Delay when LED fully lit (ms): " + String(valueA1) + " ||| Delay when LED fully dark (ms): " + String(valueA2));
 }
