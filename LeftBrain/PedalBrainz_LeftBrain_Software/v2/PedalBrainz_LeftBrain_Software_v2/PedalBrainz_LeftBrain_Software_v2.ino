0138b21c78af88b1f217fb6c53d51b5c2e6134f5
PedalBrainz <53237063+lostchocolatelab@users.noreply.github.com>
2022-08-02 17:49:52 -0700

    Plotter off

--- LeftBrain/PedalBrainz_LeftBrain_Software/v2/PedalBrainz_LeftBrain_Software_v2/PedalBrainz_LeftBrain_Software_v2.ino
+++ LeftBrain/PedalBrainz_LeftBrain_Software/v2/PedalBrainz_LeftBrain_Software_v2/PedalBrainz_LeftBrain_Software_v2.ino
@@ -598,7 +598,7 @@ void setup() {
   initial = true;
 
   setRandoms();
-  plotterPrint = true; // This prints to the Serial.Plotter and when set to True can slow down some Modez. Set to False to remove Serial Plotting of Values
+  plotterPrint = false; // This prints to the Serial.Plotter and when set to True can slow down some Modez. Set to False to remove Serial Plotting of Values
   startMillis = millis();  //initial start time
 }
 
